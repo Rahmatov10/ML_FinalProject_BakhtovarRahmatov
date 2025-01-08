291611f426b7048ff34f695bea86db43d60b3dd6
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 03:24:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 st.title('Final Project ML Course. Bakhtovar Rahmatov. Loan approval classifier')
 
 st.write('Hello world!')
