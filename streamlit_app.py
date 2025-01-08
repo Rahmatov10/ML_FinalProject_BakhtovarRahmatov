8923837a0de14b599ceedf686ba2899c08483a11
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 04:10:15 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,4 @@ st.write('Hello world!')
 
 df = pd.read_csv('https://raw.githubusercontent.com/Rahmatov10/ML_FinalProject_BakhtovarRahmatov/refs/heads/master/Loan%20-%20Loan.csv')
 
-with st.expander('Data'):
-  st.write("X")
-  X_raw = df.drop('loan_status', axis=1)
-  st.dataframe(X_raw)
-
-  st.write("y")
-  y_raw = df.loan_status
-  st.dataframe(y_raw)
-
-
-with st.sidebar:
-  st.header("Введите признаки: ")
-  st.header("Введите признаки: ")
-  island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
-  bill_depth_mm = st.slider('Bill length (mm)', 13.1, 21.5, 17.3)
-  flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
-  body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
-  gender = st.selectbox('Gender', ('female', 'male'))
+print(df)
