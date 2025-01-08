c732f4a292a362ae0717a504dd8a624b4a05a7bd
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 03:35:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ st.title('Final Project ML Course. Bakhtovar Rahmatov. Loan approval classifier'
 
 st.write('Hello world!')
 # Загрузка данных
-with st.expander('Исходные данные'):
-    df = pd.read_csv('Loan - Loan.csv')
-    
-    st.write('**X**')
-    X_raw = df.drop('loan_status', axis=1)  # 'left' - целевая переменная (увольнение)
-    X_raw
 
-    st.write('**y**')
-    y_raw = df[loan_status]
-    y_raw
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+with st.expander('Data'):
+  st.write("X")
+  X_raw = df.drop('species', axis=1)
+  st.dataframe(X_raw)
+
+  st.write("y")
+  y_raw = df.species
+  st.dataframe(y_raw)
