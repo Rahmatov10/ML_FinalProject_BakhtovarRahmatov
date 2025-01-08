9264f2c0ad5b83ad08d5562a9cda7435c3baae48
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 03:56:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,10 @@ df = pd.read_csv('Loan - Loan.csv')
 
 with st.sidebar:
   st.header("Введите признаки: ")
+  st.header("Введите признаки: ")
+  island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
+  bill_depth_mm = st.slider('Bill length (mm)', 13.1, 21.5, 17.3)
+  flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
+  body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
+  gender = st.selectbox('Gender', ('female', 'male'))
