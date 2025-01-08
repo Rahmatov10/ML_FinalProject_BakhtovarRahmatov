75f16f943cb95a04077f87af958c5d20a95ed810
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 03:37:35 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,3 @@ st.write('Hello world!')
 # Загрузка данных
 
 df = pd.read_csv("https://raw.githubusercontent.com/Rahmatov10/ML_FinalProject_BakhtovarRahmatov/refs/heads/master/Loan%20-%20Loan.csv")
-with st.expander('Data'):
-  st.write("X")
-  X_raw = df.drop('loan_status', axis=1)
-  st.dataframe(X_raw)
-
-  st.write("y")
-  y_raw = df.loan_status
-  st.dataframe(y_raw)
