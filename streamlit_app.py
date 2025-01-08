abf43d522b72de12db5e6ced4a58fe920132403f
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 03:25:24 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ st.title('Final Project ML Course. Bakhtovar Rahmatov. Loan approval classifier'
 st.write('Hello world!')
 # Загрузка данных
 with st.expander('Исходные данные'):
-    df = pd.read_csv('Loan-Loan.csv')
+    df = pd.read_csv('Loan - Loan.csv')
