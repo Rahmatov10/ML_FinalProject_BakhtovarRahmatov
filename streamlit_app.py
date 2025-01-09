0fa802fd3605f92f4bd23eb8ab8d5d379cd1d58c
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 12:00:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,20 @@ with st.expander('Визуализация данных'):
     st.bar_chart(data=df['person_gender'].value_counts())
     st.bar_chart(data=df['loan_intent'].value_counts())
 
+# Собираем вводные данные в DataFrame
+    data = {'person_age': person_age,
+            'person_gender': person_gender,
+            'person_income': person_income,
+            'person_education': person_education,
+            'person_emp_exp': person_emp_exp,
+            'person_home_ownership': person_home_ownership,
+            'loan_amnt': loan_amnt,
+            'loan_intent': loan_intent,
+            'loan_int_rate': loan_int_rate,
+            'loan_pencent_income': loan_pencent_income,
+            'cb_person_cred_hist_length': cb_person_cred_hist_length,
+            'credit_score':credit_score,
+            'previous_loan_defaults_on_file': previous_loan_defaults_on_file}  # Добавлен пол
+    input_df = pd.DataFrame(data, index=[0])
+
   
