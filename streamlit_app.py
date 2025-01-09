781c280dba1b1d34b97be21617e59c7e521a35be
Rahmatov10 <bakht.rahmatov@gmail.com>
2025-01-09 11:14:21 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,23 +19,11 @@ with st.expander('Data'):
   y_raw = df.loan_status
   st.dataframe(y_raw)
 
-# Визуализация данных
-# List of numerical columns to plot
-numerical_columns = ['person_age', 'person_income', 'person_emp_exp', 'loan_amnt', 
-                     'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length', 
-                     'credit_score']
+with st.sidebar:
+  st.header("Введите признаки: ")
+  person_age = st.slider("age (y)", 18, 50, 99)
+ 
 
-# Create a figure with subplots
-fig, axs = plt.subplots(3, 3, figsize=(15, 15))  # Adjust the number of rows and columns as needed
-axs = axs.flatten()  # Flatten the axes for easy iteration
 
-# Plot histograms for each numerical column
-for i, col in enumerate(numerical_columns):
-    sns.histplot(data[col], kde=True, ax=axs[i], bins=30)  # You can adjust the number of bins
-    axs[i].set_title(f"Distribution of {col}")
-    axs[i].set_xlabel(col)
-    axs[i].set_ylabel("Frequency")
 
-# Tight layout to avoid overlapping labels
-plt.tight_layout()
-plt.show()
+ 13  loan_status
